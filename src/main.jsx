3067ee7793c048af5826993bc54a0711488d59a5
Atharva Ombase <94031822+atharvaombase@users.noreply.github.com>
2025-01-29 23:27:35 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/routes/App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import FormContainer from "./components/forms/FormContainer";
-import { path } from "framer-motion/client";
+// import { path } from "framer-motion/client";
 import Homepage from "./components/routes/Homepage.jsx";
-
 const router = createBrowserRouter([
   {
     path: "/",
